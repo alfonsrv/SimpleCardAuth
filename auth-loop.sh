34ff7a96cbbdba1c7e5ee1ea4e3c999cf0a5eda7
alfonsrv <alfonsrau@googlemail.com>
2021-09-16 14:03:47 +0200

    Sense-making

--- auth-loop.sh
+++ auth-loop.sh
@@ -11,6 +11,7 @@ GPIO_DB_CLOSE_PATH=/sys/class/gpio/gpio$GPIO_DB_CLOSE_PIN
 
 cd "`dirname $0`"
 
+
 init_door() {
 	echo $GPIO_STRIKE_PIN > /sys/class/gpio/export
 	echo $GPIO_DB_OPEN_PIN > /sys/class/gpio/export
@@ -31,6 +32,49 @@ unlock_door() {
 	) &
 }
 
+
+#logToDB logs Information about Identity contained in the certificte into the database. So one is able to reconstruct who got when Access. 
+logToDB() {
+	#walk through multilined subject (line by line) and extract Identity-values
+	LINE_NR="0"
+	if [ -e $CERT_FILE ]; then
+		while true
+		do	
+			LINE_NR=$(($LINE_NR+1))
+			#'subject=' means one has reached the first line (the loop is starting at the most bottom line)
+			if [ $(printf $(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1)) = 'subject=' 1> /dev/null ]; then break
+			else	
+				if openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "emailAddress=" 1> /dev/null; then
+					EMAIL=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*emailAddress=//')
+				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "commonName=" 1> /dev/null; then
+					COMMONNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*commonName=//') 1> /dev/null
+				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "organizationalUnitName=" 1> /dev/null; then
+					ORGANIZATIONALUNITNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*organizationalUnitName=//')
+				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "organizationName=" 1> /dev/null; then
+					ORGANIZATIONNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*organizationName=//')
+				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "localityName=" 1> /dev/null; then
+					LOCALITYNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*localityName=//')
+				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "stateOrProvinceName=" 1> /dev/null; then
+					STATEORPROVINCENAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*stateOrProvinceName=//')
+				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "countryName=" 1> /dev/null; then
+					COUNTRYNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*countryName=//')
+				fi 
+			fi
+		done
+	fi
+	
+	#look wether Identity tried to get Access somewhere in the past
+	IDofExistingIdentity=$(sqlite3 ./logDB.sqlite "Select ID from Identity WHERE email='$EMAIL' AND CommonName='$COMMONNAME' AND OrganizationalUnitName='$ORGANIZATIONALUNITNAME' AND OrganizationName='$ORGANIZATIONNAME' AND LocalityName='$LOCALITYNAME' AND StateOrProvinceName ='$STATEORPROVINCENAME' AND CountryName='$COUNTRYNAME'")
+	#if Identity hasn´t tried to get Access in the past, insert it and look up the ID under which it was inserted
+	if (test -z $IDofExistingIdentity)
+	then sqlite3 ./logDB.sqlite "Insert INTO Identity (email, CommonName, OrganizationalUnitName, OrganizationName, LocalityName, StateOrProvinceName, CountryName) VALUES ('$EMAIL', '$COMMONNAME', '$ORGANIZATIONALUNITNAME', '$ORGANIZATIONNAME', '$LOCALITYNAME', '$STATEORPROVINCENAME', '$COUNTRYNAME');"
+	IDofExistingIdentity=$(sqlite3 ./logDB.sqlite "Select ID from Identity WHERE email='$EMAIL' AND CommonName='$COMMONNAME' AND OrganizationalUnitName='$ORGANIZATIONALUNITNAME' AND OrganizationName='$ORGANIZATIONNAME' AND LocalityName='$LOCALITYNAME' AND StateOrProvinceName ='$STATEORPROVINCENAME' AND CountryName='$COUNTRYNAME'")
+	fi
+	#Actual Logging into database
+	sqlite3 ./logDB.sqlite "Insert INTO AccessAttempt (Identity_ID, timestamp, success)  VALUES ('$IDofExistingIdentity', CURRENT_TIMESTAMP, '$1');";
+}
+
+
 lock_door() {
 	echo in > $GPIO_STRIKE_PATH/direction
 }
@@ -45,6 +89,7 @@ access_denied() {
 
 	# Beep three times with red LED to indicate failure
 	# opensc-tool --send-apdu FF:00:40:5D:04:01:01:03:01 > /dev/null 2> /dev/null
+	# opensc-tool --send-apdu FF:00:40:5D:04:01:01:02:01 beep twice
 	# sleep 1
 }
 
--- simple-card-auth.sh
+++ simple-card-auth.sh
@@ -11,12 +11,14 @@ SUBJECT_DN_FILE=subject-dn.tmp
 SERIAL_FILE=serial.tmp
 TMP_FILE=temporary.tmp
 
-CACHE_DIR=`pwd`/card-cache
 
 STRICT_CHECK=1
-KEY_ID=4
+# Where to look for PIV Certificate
+KEY_ID=1
 RAND_FILE=/dev/urandom
 STDERR=/dev/null
+
+CACHE_DIR=`pwd`/card-cache
 export OPENSC_CONF=`pwd`/opensc.conf
 
 if [ "0$DEBUG" -gt 2 ]
@@ -30,6 +32,7 @@ then
 fi
 
 cleanup() {
+	# Clean-up temporary files
 	rm $CHAL_FILE 2> /dev/null
 	rm $CERT_FILE 2> /dev/null
 	rm $CHALHASH_FILE 2> /dev/null
@@ -65,9 +68,11 @@ uncache_cert() {
 
 die() {
 	grep -v -e "Sending" -e "Receiv" "$SERIAL_FILE" | tail -n 1 2> "$STDERR"
+	
 	#log the failed AccessAttempt
 	#echo logging
-	logToDB 0
+	#logToDB 0
+	
 	[ "0$DEBUG" -lt 1 ] && cleanup
 
 	#AntiBruteforce-Timelock
@@ -86,49 +91,6 @@ die() {
 	exit 1
 }
 
-#logToDB logs Information about Identity contained in the certificte into the database. So one is able to reconstruct who got when Access. 
-logToDB() {
-	#walk through multilined subject (line by line) and extract Identity-values
-	LINE_NR="0"
-	if [ -e $CERT_FILE ]; then
-		while true
-		do	
-			LINE_NR=$(($LINE_NR+1))
-			#'subject=' means one has reached the first line (the loop is starting at the most bottom line)
-			if [ $(printf $(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1)) = 'subject=' 1> /dev/null ]; then break
-			else	
-				if openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "emailAddress=" 1> /dev/null; then
-					EMAIL=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*emailAddress=//')
-				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "commonName=" 1> /dev/null; then
-					COMMONNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*commonName=//') 1> /dev/null
-				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "organizationalUnitName=" 1> /dev/null; then
-					ORGANIZATIONALUNITNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*organizationalUnitName=//')
-				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "organizationName=" 1> /dev/null; then
-					ORGANIZATIONNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*organizationName=//')
-				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "localityName=" 1> /dev/null; then
-					LOCALITYNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*localityName=//')
-				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "stateOrProvinceName=" 1> /dev/null; then
-					STATEORPROVINCENAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*stateOrProvinceName=//')
-				elif openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | grep "countryName=" 1> /dev/null; then
-					COUNTRYNAME=$(openssl x509 -in $CERT_FILE -noout -subject -nameopt sep_multiline,lname | tail -$LINE_NR | head -1 | sed 's/\.*countryName=//')
-				fi 
-			fi
-		done
-	fi
-	
-	#look wether Identity tried to get Access somewhere in the past
-	IDofExistingIdentity=$(sqlite3 ./logDB.sqlite "Select ID from Identity WHERE email='$EMAIL' AND CommonName='$COMMONNAME' AND OrganizationalUnitName='$ORGANIZATIONALUNITNAME' AND OrganizationName='$ORGANIZATIONNAME' AND LocalityName='$LOCALITYNAME' AND StateOrProvinceName ='$STATEORPROVINCENAME' AND CountryName='$COUNTRYNAME'")
-	#if Identity hasn´t tried to get Access in the past, insert it and look up the ID under which it was inserted
-	if (test -z $IDofExistingIdentity)
-	then sqlite3 ./logDB.sqlite "Insert INTO Identity (email, CommonName, OrganizationalUnitName, OrganizationName, LocalityName, StateOrProvinceName, CountryName) VALUES ('$EMAIL', '$COMMONNAME', '$ORGANIZATIONALUNITNAME', '$ORGANIZATIONNAME', '$LOCALITYNAME', '$STATEORPROVINCENAME', '$COUNTRYNAME');"
-	IDofExistingIdentity=$(sqlite3 ./logDB.sqlite "Select ID from Identity WHERE email='$EMAIL' AND CommonName='$COMMONNAME' AND OrganizationalUnitName='$ORGANIZATIONALUNITNAME' AND OrganizationName='$ORGANIZATIONNAME' AND LocalityName='$LOCALITYNAME' AND StateOrProvinceName ='$STATEORPROVINCENAME' AND CountryName='$COUNTRYNAME'")
-	fi
-	#Actual Logging into database
-	sqlite3 ./logDB.sqlite "Insert INTO AccessAttempt (Identity_ID, timestamp, success)  VALUES ('$IDofExistingIdentity', CURRENT_TIMESTAMP, '$1');";
-}
-
-
-
 cleanup
 
 # Get ATR
@@ -141,14 +103,20 @@ dd if="$RAND_FILE" of="$CHAL_FILE" bs=32 count=1 2> "$STDERR" || die
 openssl sha256 -sha256 -binary < "$CHAL_FILE" > "$CHALHASH_FILE" || die
 
 # Get Serial
+# https://docs.yubico.com/yesdk/users-manual/application-piv/apdu/get-data.html
+# https://docs.yubico.com/yesdk/users-manual/application-piv/apdu/metadata.html
+# Get Serial Hardware S/N Yubikey 00:f8:00:00
 opensc-tool -w --send-apdu FFCA000000 --send-apdu 00:a4:04:00:09:a0:00:00:03:08:00:00:10:00 --send-apdu 00:cb:3f:ff:05:5C:03:5f:C1:02 2> $STDERR > "$SERIAL_FILE" || die
 #opensc-tool --serial --send-apdu FFCA000000 2> $STDERR > "$SERIAL_FILE" || die
 
 # See if the card is in the cache, and if so load it up.
-cache_lookup || {
+#cache_lookup || {
 	# Extract the certificate
-	pkcs15-tool $PKCS15_CRYPT_FLAGS -L --read-certificate "$KEY_ID" -o "$CERT_FILE" > "$STDERR" 2> "$STDERR" || die
-}
+#	pkcs15-tool $PKCS15_CRYPT_FLAGS -L --read-certificate "$KEY_ID" -o "$CERT_FILE" > "$STDERR" 2> "$STDERR" || die
+#}
+
+# Export Certificate
+pkcs15-tool $PKCS15_CRYPT_FLAGS -L --read-certificate "$KEY_ID" -o "$CERT_FILE"
 
 # Calculate the response for the challenge
 if ( openssl x509 -in cert.pem -text | grep -q -s "Public Key Algorithm: id-ecPublicKey" ) ;
